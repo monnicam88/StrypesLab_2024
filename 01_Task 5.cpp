d551fa9f311839ab88ace149c7664b14249ef878
monnica_88 <72393725+monnicam88@users.noreply.github.com>
2024-10-20 00:12:04 +0300

    Update 01_Task 5.cpp
    Fixed task condition misunderstanding.

--- 01_Task 5.cpp
+++ 01_Task 5.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
 int main() {
-    const double EXCHANGE_RATE_LEVA_TO_EURO = 1.95583;
-    double leva = 0.0, euro = 0.0;
+    double leva = 0.0, exchangeRateBGNtoEUR = 1.95583;
 
     std::cout << "Input monetary amount in leva: ";
     std::cin >> leva;
-    std::cout << "The monetary amount exchanged in euro is: " << (leva / EXCHANGE_RATE_LEVA_TO_EURO) << std::endl;
+    std::cout << "The monetary amount exchanged in euro is: " << (leva / exchangeRateBGNtoEUR) << std::endl;
 
     return 0;
 }
